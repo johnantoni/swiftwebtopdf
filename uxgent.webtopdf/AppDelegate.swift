d0a26f58272686521224e41ac8c84d65d5ec1ec5
John Griffiths <john@uxgent.co>
2016-07-02 21:24:11 -0400

    init.

---
+++ uxgent.webtopdf/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  uxgent.webtopdf
+//
+//  Created by John Griffiths on 2016-07-02.
+//  Copyright © 2016 uxgent. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+//    @IBOutlet weak var window: NSWindow!
+//    var viewController : ViewController!
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+//        self.viewController = ViewController(nibName:"ViewController", bundle:nil)
+//        self.window.contentView = self.viewController.view
+//        self.window.contentViewController = self.viewController
+//        self.viewController.view.frame = (self.window.contentView?.bounds)!;
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ uxgent.webtopdf/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  uxgent.webtopdf
+//
+//  Created by John Griffiths on 2016-07-02.
+//  Copyright © 2016 uxgent. All rights reserved.
+//
+
+import Cocoa
+import WebKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var containerView: NSView!
+    
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let webView = WKWebView(frame: self.view.frame)
+        
+        // load html string (working)
+        //webView.loadHTMLString("<html><body><p>Hello, World!</p></body></html>", baseURL: nil)
+        //self.view.addSubview(webView)
+        
+        // load url (working)
+        //let url = NSURL(string: "https://www.google.ca")
+        //let req = NSURLRequest(URL: url!)
+        //webView.loadRequest(req)
+        //self.view.addSubview(webView)
+
+        // load file as string (working)
+        //let fileAsString = readFromDocumentsFile("/uxgent.webtopdf.app/Contents/Resources/www/index.html")
+        //webView.loadHTMLString(fileAsString, baseURL: nil)
+        //self.view.addSubview(webView)
+        
+        //load a local html file (working)
+        let localfilePath = NSBundle.mainBundle().URLForResource("www/index", withExtension: "html");
+        let req = NSURLRequest(URL: localfilePath!);
+        webView.loadRequest(req)
+        self.view.addSubview(webView)
+    }
+    
+}
+
